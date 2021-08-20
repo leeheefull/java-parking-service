cf64ffe9ae797db717a6a991fb26f75069a29d5a
leeheechan <coolzz7@naver.com>
2021-08-20 14:52:08 +0900

    feat: equals and hashcode 추가

--- src/main/java/vehicle/Vehicle.java
+++ src/main/java/vehicle/Vehicle.java
@@ -1,5 +1,7 @@
 package vehicle;
 
+import java.util.Objects;
+
 public class Vehicle {
     private final VehicleNo no;
     private final VehicleType type;
@@ -15,5 +17,18 @@ public class Vehicle {
 
     public VehicleType getType() {
         return this.type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vehicle vehicle = (Vehicle) o;
+        return Objects.equals(no, vehicle.no) && type == vehicle.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(no, type);
     }
 }
