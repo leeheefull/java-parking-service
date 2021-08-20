2c8be6f101cf037c4763994f4a74315591c96f64
leeheechan <coolzz7@naver.com>
2021-08-20 19:53:11 +0900

    feat: 주차장 주차, 출차 구현

---
+++ src/main/java/parking/ParkingLot.java
@@ -0,0 +1,81 @@
+package parking;
+
+import vehicle.Vehicle;
+import vehicle.VehicleType;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParkingLot {
+    private final Map<ParkingPosition, Vehicle> vehicles;
+
+    private static final int PARKING_LOT_MAX_CNT = 75;
+
+    public ParkingLot() {
+        this.vehicles = new HashMap<>();
+    }
+
+    public Map<ParkingPosition, Vehicle> getVehicles() {
+        return this.vehicles;
+    }
+
+    public void park(ParkingPosition parkingPosition, Vehicle vehicle) {
+        validateDuplication(vehicle);
+        isExistence(parkingPosition);
+        validateType(parkingPosition, vehicle);
+        validateFull();
+        this.vehicles.put(parkingPosition, vehicle);
+    }
+
+    public void takeOut(Vehicle vehicle) {
+        validateEqualExistence(vehicle);
+        this.vehicles.values().removeAll(Collections.singleton(vehicle));
+    }
+
+    private void validateDuplication(Vehicle vehicle) {
+        if (isContainVehicle(vehicle)) {
+            throw new RuntimeException("해당 차량은 이미 주차되어있는 상태입니다.");
+        }
+    }
+
+    private void isExistence(ParkingPosition parkingPosition) {
+        if (isOccupy(parkingPosition)) {
+            throw new RuntimeException("해당 위치에는 이미 주차 되어있는 차량이 있습니다.");
+        }
+    }
+
+    private void validateType(ParkingPosition parkingPosition, Vehicle vehicle) {
+        if (isEqualType(parkingPosition, vehicle)) {
+            throw new RuntimeException("차량 타입에 맞지 않는 주차 지역입니다.");
+        }
+    }
+
+    private void validateFull() {
+        if (isFull()) {
+            throw new RuntimeException("현재 주차장이 꽉찬 상태입니다.");
+        }
+    }
+
+    private void validateEqualExistence(Vehicle vehicle) {
+        if (!isContainVehicle(vehicle)) {
+            throw new RuntimeException("주차 되어있지 않은 차량입니다.");
+        }
+    }
+
+    private boolean isContainVehicle(Vehicle vehicle) {
+        return this.vehicles.containsValue(vehicle);
+    }
+
+    private boolean isOccupy(ParkingPosition parkingPosition) {
+        return this.vehicles.containsKey(parkingPosition);
+    }
+
+    private boolean isEqualType(ParkingPosition parkingPosition, Vehicle vehicle) {
+        return parkingPosition.getType() != vehicle.getType() && vehicle.getType() != VehicleType.SMALL;
+    }
+
+    private boolean isFull() {
+        return this.vehicles.size() == PARKING_LOT_MAX_CNT;
+    }
+}
---
+++ src/test/java/parking/ParkingLotTest.java
@@ -0,0 +1,126 @@
+package parking;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import vehicle.Vehicle;
+import vehicle.VehicleNo;
+import vehicle.VehicleSize;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ParkingLotTest {
+    @Test
+    @DisplayName("주차가 정상적으로 될 때")
+    void right_park() {
+        // given
+        ParkingPosition parkingPosition = new ParkingPosition("1S3");
+        Vehicle vehicle = new Vehicle("1234", 1);
+        ParkingLot parkingLot = new ParkingLot();
+
+        // when
+        parkingLot.park(parkingPosition, vehicle);
+        String VehicleNo = parkingLot.getVehicles().get(new ParkingPosition("1S3")).getNo().getNo();
+
+        // then
+        assertEquals("1234", VehicleNo);
+    }
+
+    @Test
+    @DisplayName("경차의 특권")
+    void right_small_park() {
+        // given
+        ParkingPosition parkingPosition = new ParkingPosition("1M3");
+        Vehicle vehicle = new Vehicle("1234", 1);
+        ParkingLot parkingLot = new ParkingLot();
+
+        // when
+        parkingLot.park(parkingPosition, vehicle);
+        String VehicleNo = parkingLot.getVehicles().get(new ParkingPosition("1M3")).getNo().getNo();
+
+        // then
+        assertEquals("1234", VehicleNo);
+    }
+
+    @Test
+    @DisplayName("출차가 정상적으로 될 때")
+    void right_take_out() {
+        // given
+        ParkingPosition parkingPosition = new ParkingPosition("1S3");
+        Vehicle vehicle = new Vehicle("1234", 1);
+        ParkingLot parkingLot = new ParkingLot();
+
+        // when
+        parkingLot.park(parkingPosition, vehicle);
+        parkingLot.takeOut(vehicle);
+
+        // then
+        assertEquals(0, parkingLot.getVehicles().size());
+    }
+
+    @Test
+    @DisplayName("이미 같은 차량이 주차 되어있을 때")
+    void wrong_park1() {
+        // given
+        ParkingPosition parkingPosition = new ParkingPosition("1S3");
+        Vehicle vehicle = new Vehicle("1234", 1);
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.park(parkingPosition, vehicle);
+
+        // when, then
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            parkingLot.park(
+                    new ParkingPosition("1S3"),
+                    vehicle
+            );
+        });
+        assertEquals("해당 차량은 이미 주차되어있는 상태입니다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("차량 타입이 맞지 않은 주자 공간에 주차를 했을 때")
+    void wrong_park2() {
+        // given
+        ParkingPosition parkingPosition = new ParkingPosition("1M3");
+        Vehicle vehicle = new Vehicle("1234", 3);
+        ParkingLot parkingLot = new ParkingLot();
+
+        // when, then
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            parkingLot.park(parkingPosition, vehicle);
+        });
+        assertEquals("차량 타입에 맞지 않는 주차 지역입니다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("이미 다른 차량이 주차한 곳에 주차를 할 때")
+    void wrong_park4() {
+        // given
+        ParkingLot parkingLot = new ParkingLot();
+
+        ParkingPosition parkingPosition1 = new ParkingPosition("1L3");
+        Vehicle vehicle1 = new Vehicle("1234", 3);
+        parkingLot.park(parkingPosition1, vehicle1);
+
+        ParkingPosition parkingPosition2 = new ParkingPosition("1L3");
+        Vehicle vehicle2 = new Vehicle("4321", 3);
+
+        // when, then
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            parkingLot.park(parkingPosition2, vehicle2);
+        });
+        assertEquals("해당 위치에는 이미 주차 되어있는 차량이 있습니다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("주차 되어있지 않은 차량을 출차하려고 할 때")
+    void wrong_take_out() {
+        // given
+        ParkingLot parkingLot = new ParkingLot();
+        Vehicle vehicle = new Vehicle("1234", 3);
+
+        // when, then
+        Exception exception = assertThrows(RuntimeException.class, () -> parkingLot.takeOut(vehicle));
+        assertEquals("주차 되어있지 않은 차량입니다.", exception.getMessage());
+    }
+}
