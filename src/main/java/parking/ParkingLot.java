9e84af2dfcfa98ef7e2d4cd5e1946df5d3d19c83
leeheechan <coolzz7@naver.com>
2021-08-25 00:16:21 +0900

    refactor: ParkingLot validation 수정

--- src/main/java/parking/ParkingLot.java
+++ src/main/java/parking/ParkingLot.java
@@ -21,49 +21,37 @@ public class ParkingLot {
     }
 
     public void park(ParkingPosition parkingPosition, Vehicle vehicle) {
-        validateDuplication(vehicle);
-        validateExistence(parkingPosition);
-        validateType(parkingPosition, vehicle);
-        validateFull();
+        parkValidation(parkingPosition, vehicle);
         this.vehicles.put(parkingPosition, vehicle);
     }
 
     public void takeOut(Vehicle vehicle) {
-        validateEqualExistence(vehicle);
+        takeOutValidation(vehicle);
         this.vehicles.values().removeAll(Collections.singleton(vehicle));
     }
 
-    private void validateDuplication(Vehicle vehicle) {
-        if (isContainVehicle(vehicle)) {
+    private void parkValidation(ParkingPosition parkingPosition, Vehicle vehicle) {
+        if (isContain(vehicle)) {
             throw new RuntimeException("해당 차량은 이미 주차되어있는 상태입니다.");
         }
-    }
-
-    private void validateExistence(ParkingPosition parkingPosition) {
         if (isOccupy(parkingPosition)) {
             throw new RuntimeException("해당 위치에는 이미 주차 되어있는 차량이 있습니다.");
         }
-    }
-
-    private void validateType(ParkingPosition parkingPosition, Vehicle vehicle) {
-        if (isEqualType(parkingPosition, vehicle)) {
+        if (isDifferentTypeExceptSmallVehicle(parkingPosition, vehicle)) {
             throw new RuntimeException("차량 타입에 맞지 않는 주차 지역입니다.");
         }
-    }
-
-    private void validateFull() {
         if (isFull()) {
-            throw new RuntimeException("현재 주차장이 꽉찬 상태입니다.");
+            throw new RuntimeException("현재 주차장이 꽉 찬 상태입니다.");
         }
     }
 
-    private void validateEqualExistence(Vehicle vehicle) {
-        if (!isContainVehicle(vehicle)) {
+    private void takeOutValidation(Vehicle vehicle) {
+        if (!isContain(vehicle)) {
             throw new RuntimeException("주차 되어있지 않은 차량입니다.");
         }
     }
 
-    private boolean isContainVehicle(Vehicle vehicle) {
+    private boolean isContain(Vehicle vehicle) {
         return this.vehicles.containsValue(vehicle);
     }
 
@@ -71,7 +59,7 @@ public class ParkingLot {
         return this.vehicles.containsKey(parkingPosition);
     }
 
-    private boolean isEqualType(ParkingPosition parkingPosition, Vehicle vehicle) {
+    private boolean isDifferentTypeExceptSmallVehicle(ParkingPosition parkingPosition, Vehicle vehicle) {
         return parkingPosition.getType() != vehicle.getType() && vehicle.getType() != VehicleType.SMALL;
     }
 
